13e677148633c0556f01ef9084279ceefd46db44
ftnext <takuyafjp+develop@gmail.com>
2024-09-21 15:27:09 +0900

    [feat] Distinguish index pages

--- hatch-example/polls_tutorial/polls/views.py
+++ hatch-example/polls_tutorial/polls/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world (from Hatch-example). You're at the polls index.")
